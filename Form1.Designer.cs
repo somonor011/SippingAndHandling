a648131d7323656b855399132d3fea8d1af76aed
somonor011 <somonorhong011@gmail.com>
2024-10-31 22:10:29 +0700

    color design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,20 +45,20 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(46, 55);
+            label1.Font = new Font("Segoe UI Black", 10.2F, FontStyle.Bold);
+            label1.Location = new Point(46, 62);
             label1.Name = "label1";
-            label1.Size = new Size(86, 20);
+            label1.Size = new Size(107, 23);
             label1.TabIndex = 0;
             label1.Text = "Order Total";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(46, 101);
+            label2.Font = new Font("Segoe UI Black", 10.2F, FontStyle.Bold);
+            label2.Location = new Point(46, 108);
             label2.Name = "label2";
-            label2.Size = new Size(336, 20);
+            label2.Size = new Size(416, 23);
             label2.TabIndex = 1;
             label2.Text = "Customer Type (P=Preferred N=non Prerferred)";
             label2.Click += label2_Click;
@@ -66,20 +66,20 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(46, 145);
+            label3.Font = new Font("Segoe UI Black", 10.2F, FontStyle.Bold);
+            label3.Location = new Point(46, 152);
             label3.Name = "label3";
-            label3.Size = new Size(307, 20);
+            label3.Size = new Size(379, 23);
             label3.TabIndex = 2;
             label3.Text = "Shipping Cost (Free for preferred customer)";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(46, 190);
+            label4.Font = new Font("Segoe UI Black", 10.2F, FontStyle.Bold);
+            label4.Location = new Point(46, 197);
             label4.Name = "label4";
-            label4.Size = new Size(105, 20);
+            label4.Size = new Size(134, 23);
             label4.TabIndex = 3;
             label4.Text = "Sales Tax (7%)";
             label4.Click += label4_Click;
@@ -87,17 +87,17 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(46, 238);
+            label5.Font = new Font("Segoe UI Black", 10.2F, FontStyle.Bold);
+            label5.Location = new Point(46, 245);
             label5.Name = "label5";
-            label5.Size = new Size(88, 20);
+            label5.Size = new Size(110, 23);
             label5.TabIndex = 4;
             label5.Text = "Grand Total";
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(403, 55);
+            textBox1.Location = new Point(503, 62);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(125, 30);
             textBox1.TabIndex = 5;
@@ -105,7 +105,7 @@
             // textBox2
             // 
             textBox2.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox2.Location = new Point(403, 101);
+            textBox2.Location = new Point(503, 108);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(47, 30);
             textBox2.TabIndex = 6;
@@ -114,7 +114,7 @@
             // 
             textBox3.Cursor = Cursors.No;
             textBox3.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox3.Location = new Point(403, 145);
+            textBox3.Location = new Point(503, 152);
             textBox3.Name = "textBox3";
             textBox3.ReadOnly = true;
             textBox3.Size = new Size(125, 30);
@@ -124,7 +124,7 @@
             // 
             textBox4.Cursor = Cursors.No;
             textBox4.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox4.Location = new Point(403, 190);
+            textBox4.Location = new Point(503, 194);
             textBox4.Name = "textBox4";
             textBox4.ReadOnly = true;
             textBox4.Size = new Size(125, 30);
@@ -134,7 +134,7 @@
             // 
             textBox5.Cursor = Cursors.No;
             textBox5.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox5.Location = new Point(403, 238);
+            textBox5.Location = new Point(503, 238);
             textBox5.Name = "textBox5";
             textBox5.ReadOnly = true;
             textBox5.Size = new Size(125, 30);
@@ -142,28 +142,34 @@
             // 
             // button1
             // 
-            button1.Location = new Point(101, 323);
+            button1.BackColor = Color.DodgerBlue;
+            button1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(126, 311);
             button1.Name = "button1";
-            button1.Size = new Size(164, 38);
+            button1.Size = new Size(216, 41);
             button1.TabIndex = 10;
             button1.Text = "Calculate Grand Total";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            button2.Location = new Point(352, 323);
+            button2.BackColor = Color.Red;
+            button2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(377, 311);
             button2.Name = "button2";
-            button2.Size = new Size(78, 38);
+            button2.Size = new Size(102, 41);
             button2.TabIndex = 11;
             button2.Text = "Exit";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.Silver;
-            ClientSize = new Size(591, 402);
+            BackColor = Color.FromArgb(192, 192, 255);
+            ClientSize = new Size(705, 402);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(textBox5);
